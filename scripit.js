f9f96985cd793539c1c2fbdbd4e4ab79546df71d
Saikarthikaji <147479209+Saikarthikaji@users.noreply.github.com>
2023-11-09 11:45:21 +0530

    Add files via upload
    add

---
+++ scripit.js
@@ -0,0 +1,38 @@
+console.log('welcome to javascripit');
+
+
+
+//varibales
+var a; //declaration
+a=10;  //initialization
+var b="hello"  //declaration + initialization
+
+
+//datatypes
+/*
+string
+number
+boolean
+undefined
+null
+*/
+
+var str="hello world";
+console.log("str :", str);
+console.log("type of str:, type of str")
+
+var num =100;
+console.log("num :", num);
+console.log("typeof num :" , typeof num);
+
+var bool = ture;
+console.log("bool : ",bool);
+console.log("type of bool: ", typeof bool);
+
+var bool1 = false
+console.log("bool1 : ",bool1);
+console.log("type of bool1 : ", typeof bool1);
+
+var n = null;
+console.log("n : ", n);
+console.log("type of n : ", typeof n);
